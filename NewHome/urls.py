2cf7b8ad3c50ebd63c2c53ead23b3914997f2fec
Sakshi254 <115886418+Sakshi254@users.noreply.github.com>
2024-02-24 16:59:54 +0530

    Update

--- NewHome/urls.py
+++ NewHome/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('lists/',include('lists.urls')),
     path('',include('pages.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + staticfiles_urlpatterns()
